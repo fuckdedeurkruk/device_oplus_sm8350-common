76fe7efa31d5d18ff6b5947272937560f232c9ab
itsxrp <itsxrproms@gmail.com>
2022-05-25 10:14:30 +0000

    sm8350-common: Build android.hardware.graphics.composer@2.1
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- common.mk
+++ common.mk
@@ -161,6 +161,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.graphics.allocator@3.0-impl \
     android.hardware.graphics.allocator@4.0-impl \
+    android.hardware.graphics.composer@2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     libdisplayconfig.qti \
